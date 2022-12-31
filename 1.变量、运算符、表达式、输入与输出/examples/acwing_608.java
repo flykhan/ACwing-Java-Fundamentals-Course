06f763d654dbd64d4da71302ab6e09d2ba6d95ba
flykhan <liubo666666@outlook.com>
2022-12-31 15:52:20 +0800

    AxB-CxD

---
+++ 1.变量、运算符、表达式、输入与输出/examples/acwing_608.java
@@ -0,0 +1,11 @@
+package examples;
+import java.util.Scanner;
+
+/* 读取四个整数 A,B,C,D，并计算 (A×B−C×D) 的值。 */
+public class acwing_608 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt(), b = sc.nextInt(), c = sc.nextInt(), d = sc.nextInt();
+        System.out.printf("DIFERENCA = ", a * b - c * d);
+    }
+}
