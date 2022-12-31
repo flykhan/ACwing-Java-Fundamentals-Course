b1f3232df9c42807ace979109181eaabf9a8db2e
flykhan <liubo666666@outlook.com>
2022-12-31 16:17:06 +0800

    格式化

--- 1.变量、运算符、表达式、输入与输出/examples/acwing_608.java
+++ 1.变量、运算符、表达式、输入与输出/examples/acwing_608.java
@@ -6,6 +6,6 @@ public class acwing_608 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int a = sc.nextInt(), b = sc.nextInt(), c = sc.nextInt(), d = sc.nextInt();
-        System.out.printf("DIFERENCA = ", a * b - c * d);
+        System.out.printf("DIFERENCA = %d", a * b - c * d);
     }
 }
