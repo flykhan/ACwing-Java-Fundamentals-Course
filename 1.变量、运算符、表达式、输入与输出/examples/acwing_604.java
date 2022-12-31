e6dfb6886ad359b7b6a1ec9784a926b35e06c510
flykhan <liubo666666@outlook.com>
2022-12-31 16:24:04 +0800

    求圆面积

---
+++ 1.变量、运算符、表达式、输入与输出/examples/acwing_604.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+
+public class acwing_604 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        double a = sc.nextDouble();
+        System.out.printf("A=%.4f", 3.14159 * a * a);
+    }
+}
