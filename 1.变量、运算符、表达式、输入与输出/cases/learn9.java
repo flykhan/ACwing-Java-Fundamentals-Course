a71db01f9e14e77c0b1e477c83625a9babaec045
flykhan <liubo666666@outlook.com>
2023-01-04 11:41:43 +0800

    BufferedWriter和OutputStreamWriter

--- 1.变量、运算符、表达式、输入与输出/cases/learn9.java
+++ 1.变量、运算符、表达式、输入与输出/cases/learn9.java
@@ -3,7 +3,7 @@ import java.io.OutputStreamWriter;
 
 public class learn9 {
     public static void main(String[] args) throws Exception {
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));// 注意这里是System.out
         bw.write("Hello World\n");
         bw.flush();// 需要手动刷新缓冲区
     }
